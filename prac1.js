93d09bff261e844c933a78336489341421bf4653
Muhammad Aadil Chanda <aadilchanda1069@gmail.com>
2025-10-07 16:41:46 +0500

    Update prac1.js

--- prac1.js
+++ prac1.js
@@ -1,7 +1,8 @@
+"use strict"; // use all the JS code as newer version
 console.log("Helloo World");
 let accountEmail = "aadilchanda1069@gmail.com";
 let accountPass = "12345";
 let accountState = "active";
 
-accountCity = "okara";
+let accountCity = "okara";
 console.table([accountEmail, accountPass, accountState, accountCity]);
