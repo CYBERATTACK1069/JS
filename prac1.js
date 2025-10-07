2c2108d72eaa08d7bacbb659dc526510c08994cf
Muhammad Aadil Chanda <aadilchanda1069@gmail.com>
2025-10-07 16:35:24 +0500

    updated prac1.js File
    Updated File

--- prac1.js
+++ prac1.js
@@ -1,1 +1,7 @@
 console.log("Helloo World");
+let accountEmail = "aadilchanda1069@gmail.com";
+let accountPass = "12345";
+let accountState = "active";
+
+accountCity = "okara";
+console.table([accountEmail, accountPass, accountState, accountCity]);
